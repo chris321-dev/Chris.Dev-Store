57585b394a1a5d49229ec2485f7fabb1f7f7052c
chris321-dev <okaforchristian08@gmail.com>
2024-09-12 21:34:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Navbar />
           <Routes>
             {/* Redirect root path to /Chris.Dev's-Store */}
-            <Route path="/" element={<Navigate to="/Chris.Dev's-Store" />} />
+            {/* <Route path="/" element={<Navigate to="/Chris.Dev's-Store" />} /> */}
             <Route path="/Chris.Dev's-Store" element={<Home />} />
             <Route path="/shop" element={<Shop />} />            
             <Route path="/contact" element={<Contact />} />
